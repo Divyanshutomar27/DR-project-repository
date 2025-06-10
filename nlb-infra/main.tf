45bf44c4648da5674533b11a658aa20c7aecb528
Divyanshu Tomar <divyanshutomar27@gmail.com>
2025-06-10 13:37:56 +0530

    added nlb-infra

--- nlb-infra/main.tf
+++ nlb-infra/main.tf
@@ -106,6 +106,6 @@ resource "aws_lb_target_group" "ec2-tg" {
 
 resource "aws_lb_target_group_attachment" "ec2_ip_target" {
   target_group_arn = aws_lb_target_group.ec2-tg.arn
-  target_id        = data.aws_instance.application_ec2.private_ip
+  target_id        = "10.0.101.117"
   port             = 80
 }
